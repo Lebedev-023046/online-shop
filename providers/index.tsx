bc756cf9e4841ec03dd2746cc4a6974fc3c08f77
lebedev-023046 <lebedev.023046@gmail.com>
2024-01-19 17:11:31 +0300

    feat: wrap app into tailwind provider

--- providers/index.tsx
+++ providers/index.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import { ThemeProvider } from "@material-tailwind/react";
 
-export function Provider({ children }: { children: React.ReactNode }) {
+export function Providers({ children }: { children: React.ReactNode }) {
   return <ThemeProvider>{children}</ThemeProvider>;
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,19 @@
-import type { Config } from "tailwindcss";
+import withMT from "@material-tailwind/react/utils/withMT";
 
-const config: Config = {
+const config = withMT({
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    // extend: {
-    //   backgroundImage: {
-    //     "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-    //     "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-    //   },
-    // },
+    colors: {
+      carrot: "#fa5f1a",
+      light: "#ffffff",
+      soft: "#f7f0f0",
+      dark: "#252525",
+    },
   },
   plugins: [],
-};
+});
 export default config;

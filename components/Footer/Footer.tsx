fc033fe5a514a1b7e473157bc0ac956e962020cd
lebedev-023046 <lebedev.023046@gmail.com>
2024-02-03 00:16:45 +0300

    feat: add footer

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="flex h-[5rem] items-center justify-center bg-soft">
+      Footer
+    </footer>
+  );
+}

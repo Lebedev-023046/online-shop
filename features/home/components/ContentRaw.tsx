0e26eed1f7a8f7ea7681f1298be33257836cf994
lebedev-023046 <lebedev.023046@gmail.com>
2024-02-04 14:26:16 +0300

    feat: add basic content-raw

---
+++ features/home/components/ContentRaw.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+import { Card } from "./Card";
+
+export function ContentRaw() {
+  return (
+    <div>
+      <div className="flex justify-between">
+        <div>Костюмы</div>
+        <Link href={`/categories`} className="relative mb-2 flex gap-1">
+          <div className="pseudo-underline before:duration-0">
+            Все{" "}
+            <Image
+              className="inline"
+              src="/right-stroke.svg"
+              width={8}
+              height={8}
+              alt="stroke"
+            />
+          </div>
+        </Link>
+      </div>
+      <div className="grid items-center gap-6 overflow-auto sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-4">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
+}

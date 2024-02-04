ecfeb5b741a64180655fb2542a0b0659fd6b1d50
lebedev-023046 <lebedev.023046@gmail.com>
2024-02-04 18:21:55 +0300

    feat: add footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,9 +1,55 @@
+import Link from "next/link";
 import React from "react";
+
+const footerInfo = [
+  {
+    title: "Покупателям",
+    subtitle1: "Доставка",
+    subtitle2: "Возврат",
+    subtitle3: "Оплата",
+  },
+  {
+    title: "О Компании",
+    subtitle1: "О нас",
+    subtitle2: "Возврат",
+    subtitle3: "Юридическая информация",
+  },
+];
 
 export default function Footer() {
   return (
-    <footer className="h-[5rem]  bg-soft">
-      <div className="flex h-full items-center justify-center">Footer</div>
+    <footer className="border-t-2">
+      <div className="p-10">
+        <div className="flex items-start justify-center gap-20">
+          {footerInfo.map((item, index) => (
+            <div key={index}>
+              <section>
+                <h1 className="pb-4 text-lg font-medium">{item.title}</h1>
+                <div className="flex flex-col gap-2 text-sm">
+                  <Link href="##">{item.subtitle1}</Link>
+                  <Link href="##">{item.subtitle2}</Link>
+                  <Link href="##">{item.subtitle3}</Link>
+                </div>
+              </section>
+            </div>
+          ))}
+          <div className="flex max-w-[500px] flex-col gap-2">
+            <p>
+              Подпишитесь на нашу e-mail рассылку, чтобы первыми узнавать о
+              новостях и акциях MAZY
+            </p>
+            <input
+              className="w-full max-w-[20rem] bg-[#eee] p-1"
+              type="email"
+              placeholder="Введите эл. почту"
+            />
+            <div className="flex gap-5">
+              <Link href="##">Instagram</Link>
+              <Link href="##">Telegram</Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
